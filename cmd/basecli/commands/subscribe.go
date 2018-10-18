2693cdd2bba1cf515dde800d5e78407cfab3dd94
engenegr <evdokimov.ilya@gmail.com>
2018-10-18 19:12:42 +0300

    events update code (dont work). just asking for status and printing height

--- cmd/basecli/commands/subscribe.go
+++ cmd/basecli/commands/subscribe.go
@@ -4,8 +4,8 @@ import (
 	"github.com/spf13/cobra"
 
 	lcmd "github.com/tendermint/light-client/commands"
-	"github.com/tendermint/tendermint/rpc/client"
 	"fmt"
+	"github.com/tendermint/tendermint/rpc/client"
 )
 
 var AccountSubscribeCmd = &cobra.Command{
@@ -15,18 +15,54 @@ var AccountSubscribeCmd = &cobra.Command{
 }
 
 func createAccountSubscribtion(cmd *cobra.Command, args []string) error {
-	c := lcmd.GetNode()
-
+	//Events-based code. Don't work.
+	/*
+	c:= client.NewHTTP(viper.GetString("node"), "/websocket")
 	status, err := c.Status()
-	h := status.LatestBlockHeight + 1
-
+	if err != nil {
+		return err
+	}
+	// listen for a new block; ensure height increases by 1
+	var firstBlockHeight int
+	firstBlockHeight = status.LatestBlockHeight
+	fmt.Printf("Started subscription at %d", firstBlockHeight)
 	for {
-		err = client.WaitForHeight(c, h, nil)
+			evtTyp := types.EventStringNewBlock()
+			evt, err := client.WaitForOneEvent(c, evtTyp, 1*time.Second)
+			if err != nil {
+				return err
+			}
+
+			blockEvent, _ := evt.Unwrap().(types.EventDataNewBlock)
+
+			block := blockEvent.Block
+			fmt.Println(block)
+		}
+	*/
+
+	//stupid but it works and this is what Tendermint developers did at 0.10 version
+	c := lcmd.GetNode()
+	status, err := c.Status()
+	if err != nil {
+		return err
+	}
+	h := status.LatestBlockHeight
+	fmt.Printf("Started subscription at %d", h)
+	waiter := client.DefaultWaitStrategy
+	delta := 1
+	for {
+		s, err := c.Status()
 		if err != nil {
 			return err
 		}
+		last := s.LatestBlockHeight
+		if h != last { delta = 1 } else { delta +=1 }
+		// wait for the time, or abort early
+		if err := waiter(delta); err != nil {
+			return err
+		}
+		h = last
 		fmt.Printf("Chain now at height %d\n", h)
-		h = h + 1
 	}
 
 	return nil
